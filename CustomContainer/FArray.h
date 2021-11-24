9b8c5a765b16ff431f511648d77340d650fa45ed
fedor.andreev <fedorr47@gmail.com>
2021-11-24 19:24:56 +0300

    First step

---
+++ CustomContainer/FArray.h
@@ -0,0 +1,14 @@
+#pragma once
+
+template <class TElementType, typename TAllocator>
+class FArray
+{
+public:
+
+	using ElementType = TElementType;
+	using Allocator = TAllocator;
+
+protected:
+
+
+};
---
+++ CustomContainer_tests/StackAllocatorTest.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+
+#include "StackAllocatorTest.h"
+
+TEST(StackAllocatorTests, CreateStackAllocator) {
+	StackAllocator tStacAllocator(100);
+	tStacAllocator.Init();
+	int* ptrStartPointer = reinterpret_cast<int*>(tStacAllocator.GetData());
+	EXPECT_TRUE(ptrStartPointer != nullptr);
+}
+
+TEST_F(TestStackAllocator, Allocate) {
+	int* ptrStartPointer = reinterpret_cast<int*>(ptrStackAllocator->GetData());
+	EXPECT_TRUE(ptrStartPointer != nullptr);
+	void* ptrVoidAllocated = ptrStackAllocator->Allocate(sizeof(TestAllocStruct), 0);
+	int* ptrAllocated = reinterpret_cast<int*>(ptrVoidAllocated);
+	EXPECT_TRUE(ptrAllocated != nullptr && ptrAllocated > ptrStartPointer);
+	new(ptrStartPointer) TestAllocStruct();
+	TestAllocStruct* ptrTestAllocStruct = reinterpret_cast<TestAllocStruct*>(ptrStartPointer);
+	EXPECT_EQ(ptrTestAllocStruct->IntegerValue, 1);
+	EXPECT_EQ(ptrTestAllocStruct->FloatValue, 1.1f);
+	EXPECT_EQ(ptrTestAllocStruct->BoolValue, true);
+	EXPECT_EQ(ptrTestAllocStruct->ShortString, "A");
+}
+
