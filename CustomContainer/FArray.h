f792553b4b66f15c56621313ee0af15982f16fcc
Федор Андреев <fedorr47@gmail.com>
2021-11-25 21:24:33 +0300

    Small changes

--- CustomContainer/FArray.h
+++ CustomContainer/FArray.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <assert.h>
+#include "Allocator.h"
+
+#define FORCE_INLINE __forceinline
+
 template <class TElementType, typename TAllocator>
 class FArray
 {
@@ -8,7 +13,37 @@ public:
 	using ElementType = TElementType;
 	using Allocator = TAllocator;
 
+	FORCE_INLINE FArray() :
+		mNum{0},
+		mMax{0}
+	{
+	}
+
+	FORCE_INLINE const ElementType* GetData() const
+	{
+		return reinterpret_cast<ElementType>(mAllocatorInstance.GetData());
+	}
+
+	FORCE_INLINE const size_t GetTypeSize() const
+	{
+		retrun sizeof(ElementType);
+	}
+	
+	FORCE_INLINE const size_t GetOffset() const
+	{
+		return mMax - mNum;
+	}
+
+	FORCE_INLINE bool IsValidIndex(int32 InIndex) const
+	{
+		return InIndex >= 0 && InIndex < mNum;
+	}
+
+	FORCE_INLINE size_t Num() const 
+	{}
+
 protected:
-
-
+	Allocator mAllocatorInstance;
+	size_t mNum;
+	size_t mMax;
 };
